0680e2143d05703649d08f0d5813ea7340e96116
Janet Warren <33846232+jdublu416@users.noreply.github.com>
2018-03-11 17:45:29 -0400

    bug fixes and media queries added to CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,27 @@
 $(document).ready(function() {
   //researching the API and these are the key and
   var topics = [
-    "puppy",
-    "kitten",
+    "puppies",
+    "kittens",
     "baby",
     "fainting goats",
     "ducklings",
-    "piglets",
+   "monkeys",
     "bunnies"
   ];
 
   createButtons();
 
-  $(".btnGiphy").on("click", function(event) {
-      createButtons();
+  $("#buttonGroup").on("click", ".btnGiphy", function(event) {
     var userQuery = $(this).attr("data-name");
     console.log(userQuery);
+    $("#picContainer").empty();
     displayStillGif(userQuery);
+
   });
 
   function displayStillGif(userQuery) {
-
+    console.log("yup too");
     var userQuery;
     var apikey = "api_key=1oACTeC3yeklQABKSzKWqS7ojx3Npdlv";
     var queryURL =
@@ -31,7 +32,7 @@ $(document).ready(function() {
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      console.log(response);
+      
       var results = response.data;
 
       console.log(results);
@@ -39,36 +40,40 @@ $(document).ready(function() {
       for (var i = 0; i < 10; i++) {
         // Storing the rating data, create element to display it, then display rating
         var rating = results[i].rating;
-        var gifTitle=results[i].title;
-        var paraOne = $("<p>").text("Rating: " + rating +""+ "    Title: "+ gifTitle);
+        var gifTitle = results[i].title;
+        var paraOne = $("<p>").html(
+          "Rating: " + rating + "<br>" + "Title: " + gifTitle
+        );
         // displaySection.appendChild(paraOne);
         $("#picContainer").append(paraOne);
 
         //getting the https for the still gif, creating an element to hold it as the child of section,
-
         var stillGif = results[i].images.fixed_height_still.url;
+
         console.log(stillGif);
-        var animGif= results[i].images.fixed_height.url;
+        var animGif = results[i].images.fixed_height.url;
         var tagImage = $("<img class='gif'>");
         tagImage.attr("src", stillGif);
-        tagImage.attr("data-still",stillGif);
-        tagImage.attr("data-animate",animGif);
+        tagImage.attr("data-still", stillGif);
+        tagImage.attr("data-animate", animGif);
         tagImage.attr("data-state", "still");
+       
         $("#picContainer").append(tagImage);
-
-        $(".gif").on("click", function() { //state begins as "still" and then click event changes data-state to "animate"
-           
-            var state = $(this).attr("data-state");
-           
-            if (state === "still") {
-              $(this).attr("src", $(this).attr("data-animate"));
-              $(this).attr("data-state", "animate");
-            } else {
-              $(this).attr("src", $(this).attr("data-still"));
-              $(this).attr("data-state", "still");
-            }
-          });
       }
+      $("#picContainer").on("click", ".gif", function() {
+        //state begins as "still" and then click event changes data-state to "animate"
+
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+          console.log("yup");
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      });
     });
   }
 
